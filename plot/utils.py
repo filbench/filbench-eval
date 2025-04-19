321cb7959ccc02598a32b4b96b53c9fca8cfbbc2
ljvmiranda921 <ljvmiranda@gmail.com>
2025-04-19 12:47:23 -0700

    [WIP] Update

---
+++ plot/utils.py
@@ -0,0 +1,23 @@
+FONT_SIZES = {"small": 14, "medium": 18, "large": 24}
+
+PLOT_PARAMS = {
+    "font.family": "serif",
+    "font.serif": ["Times"],
+    "font.size": FONT_SIZES.get("medium"),
+    "axes.titlesize": FONT_SIZES.get("large"),
+    "axes.labelsize": FONT_SIZES.get("large"),
+    "xtick.labelsize": FONT_SIZES.get("large"),
+    "ytick.labelsize": FONT_SIZES.get("large"),
+    "legend.fontsize": FONT_SIZES.get("medium"),
+    "figure.titlesize": FONT_SIZES.get("medium"),
+    "text.usetex": True,
+}
+
+COLORS = {
+    "blue": "#D1F9F1",
+    "crest": "#FFE2C8",
+    "cherry": "#F2CAD8",
+    "purple": "#F2ECF8",
+    "green": "#DFF2EA",
+    "indigo": " #EBEDFB",
+}
