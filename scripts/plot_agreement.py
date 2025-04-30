f7abbcde57b38fbfb8a748863f8c4f40cbe18422
ljvmiranda921 <ljvmiranda@gmail.com>
2025-04-30 13:35:22 -0700

    [WIP] Update

--- scripts/plot_agreement.py
+++ scripts/plot_agreement.py
@@ -3,5 +3,14 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from datasets import load_dataset
 
 from scripts.utils import COLORS, PLOT_PARAMS
+
+plt.rcParams.update(PLOT_PARAMS)
+
+
+def main():
+    # fmt: off
+    # fmt: on
+    pass
