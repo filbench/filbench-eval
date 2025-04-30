2f69588d8fec159e8bfe66dae48ebd5c06d29fdf
ljvmiranda921 <ljvmiranda@gmail.com>
2025-04-30 13:43:35 -0700

    [WIP] Update

--- scripts/plot_agreement.py
+++ scripts/plot_agreement.py
@@ -12,5 +12,16 @@ plt.rcParams.update(PLOT_PARAMS)
 
 def main():
     # fmt: off
+    parser = argparse.ArgumentParser(description="Check agreement and plot.")
+    parser.add_argument("-m", "--model_names", nargs="+", default=[], help="Model names to check agreement on.")
+    parser.add_argument("-t", "--task", nargs="+", default=[], help="Tasks to check model agreement on.")
+    parser.add_argument("-n", "--num_samples", type=int, default=100, help="Number of samples for computing agreement (higher is more reliable).")
+    parser.add_argument("--output_path", type=Path, default="plots/impact_of_lm_size.pdf", help="Path to save the results.")
+    parser.add_argument("--figsize", type=int, nargs=2, default=[6, 6], help="Matplotlib figure size.")
+    parser.add_argument("--svg", action="store_true", default=False, help="If set, will also save an SVG version.")
+    args = parser.parse_args()
     # fmt: on
-    pass
+
+
+if __name__ == "__main__":
+    main()
