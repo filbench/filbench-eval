d2c080e12685fc997850cb193fa324a32e9dbd0a
ljvmiranda921 <ljvmiranda@gmail.com>
2025-04-30 14:25:44 -0700

    [WIP] Update

--- scripts/plot_agreement.py
+++ scripts/plot_agreement.py
@@ -10,17 +10,31 @@ from scripts.utils import COLORS, PLOT_PARAMS
 plt.rcParams.update(PLOT_PARAMS)
 
 
+def format_model(model_name: str) -> str:
+    return model_name.replace("/", "__")
+
+
+def format_task(task_name: str) -> str:
+    return task_name.replace("|", "_")
+
+
 def main():
     # fmt: off
     parser = argparse.ArgumentParser(description="Check agreement and plot.")
     parser.add_argument("-m", "--model_names", nargs="+", default=[], help="Model names to check agreement on.")
-    parser.add_argument("-t", "--task", nargs="+", default=[], help="Tasks to check model agreement on.")
+    parser.add_argument("-t", "--task_names", nargs="+", default=[], help="Tasks to check model agreement on (e.g., filbench|balita_tgl_mcf|0).")
     parser.add_argument("-n", "--num_samples", type=int, default=100, help="Number of samples for computing agreement (higher is more reliable).")
     parser.add_argument("--output_path", type=Path, default="plots/impact_of_lm_size.pdf", help="Path to save the results.")
     parser.add_argument("--figsize", type=int, nargs=2, default=[6, 6], help="Matplotlib figure size.")
     parser.add_argument("--svg", action="store_true", default=False, help="If set, will also save an SVG version.")
     args = parser.parse_args()
     # fmt: on
+
+    for model in args.model_names:
+        results_ds_name = f"UD-Filipino/details_{format_model(model)}_private"
+        for task in args.task_names:
+            ds = load_dataset(results_ds_name, task)
+            breakpoint()
 
 
 if __name__ == "__main__":
