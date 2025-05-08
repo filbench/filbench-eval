c0b1257b798ffda3458cb3886e82330be2ae66ec
ljvmiranda921 <ljvmiranda@gmail.com>
2025-05-07 20:55:05 -0700

    [WIP] Update

--- scripts/plot_price_per_model.py
+++ scripts/plot_price_per_model.py
@@ -13,25 +13,51 @@ def main():
     # fmt: off
     parser = argparse.ArgumentParser(description="Plot cost-efficiency chart.")
     parser.add_argument("--input_path", type=Path, help="Path to the leaderboard results.")
-    parser.add_argument("--price_per_token_data", type=Path, help="Path to the CSV containing per-token price.")
+    parser.add_argument("--cost_data", type=Path, help="Path to the CSV containing per-token price.")
+    parser.add_argument("--output_path", type=Path, default="plots/price_per_model.pdf", help="Path to save the results.")
     parser.add_argument("--figsize", type=int, nargs=2, default=[6, 6], help="Matplotlib figure size.")
     parser.add_argument("--svg", action="store_true", default=False, help="If set, will also save an SVG version.")
     args = parser.parse_args()
     # fmt: on
 
-    df = pd.read_csv(args.input_path)
-    df = df[df["Incomplete"]]  # It is inversed because of how toggle works in Gradio
-    df = df[df["# Parameters"] != -1]  # Only keep models where # Parameters != -1
-    df = df[["Model", "Average", "# Parameters", "Multilingual"]]
-    df = df[df["# Parameters"] <= args.max_params]
-    df = df.reset_index(drop=True)
+    scores_df = pd.read_csv(args.input_path)
+    scores_df = scores_df[scores_df["Incomplete"]]
+    scores_df = scores_df[["Model", "Average", "# Parameters", "Multilingual"]]
+    scores_df = scores_df.reset_index(drop=True)
+    price_df = pd.read_csv(args.cost_data)
+    df = price_df.merge(scores_df, on="Model", how="left")
+    df = df.sort_values(by=["Average"], ascending=False).reset_index(drop=True)
 
     fig, ax = plt.subplots(figsize=args.figsize)
     colors = {
         "Multilingual": COLORS.get("warm_blue"),
         "SEA-Focused": COLORS.get("warm_crest"),
     }
-    breakpoint()
+    for category, color in colors.items():
+        mask = df["Multilingual"] == category
+        ax.scatter(
+            df.loc[mask, "price_per_output_token"],
+            df.loc[mask, "Average"],
+            s=120,
+            alpha=0.9,
+            color=color,
+            label=category,
+        )
+
+    ax.set_xlabel("Inference Cost (\$/1M output tokens)")
+    ax.set_ylabel("FilBench Score")
+    ax.grid(color="gray", alpha=0.2, which="both")
+
+    output_path = Path(args.output_path)
+
+    plt.tight_layout()
+
+    fig.savefig(output_path, bbox_inches="tight")
+    if args.svg:
+        print("Also saving an SVG version")
+        fig.savefig(output_path.with_suffix(".svg"), bbox_inches="tight")
+
+    print(df)
 
 
 if __name__ == "__main__":
