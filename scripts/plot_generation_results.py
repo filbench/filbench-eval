530e5008bb9d1a2d3a107974f63a89f2c04db772
ljvmiranda921 <ljvmiranda@gmail.com>
2025-05-11 12:23:01 -0700

    [WIP] Update

--- scripts/plot_generation_results.py
+++ scripts/plot_generation_results.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from scripts.utils import COLORS, PLOT_PARAMS
 
+plt.rcParams.update(PLOT_PARAMS)
+
 # From Ely: https://docs.google.com/spreadsheets/d/1oW_4X1XLwFDyRhLnaq8OkZ84DaIfF-JsNy2DdTv0ji4/edit?usp=sharing
 GENERATION_RESULTS = {
     "CohereLabs/aya-expanse-32b": {
@@ -39,7 +41,46 @@ def main():
     # fmt: off
     parser = argparse.ArgumentParser(description="Plot k-shot generation results.")
     parser.add_argument("--output_path", type=Path, default="plots/generation_results.pdf", help="Path to save the results.")
-    parser.add_argument("--figsize", type=int, nargs=2, default=[6, 6], help="Matplotlib figure size.")
+    parser.add_argument("--figsize", type=int, nargs=2, default=[6, 8], help="Matplotlib figure size.")
     parser.add_argument("--svg", action="store_true", default=False, help="If set, will also save an SVG version.")
     args = parser.parse_args()
     # fmt: on
+
+    fig, ax = plt.subplots(figsize=args.figsize)
+    colors = {
+        "Multilingual": COLORS.get("warm_blue"),
+        "SEA-specific": COLORS.get("warm_crest"),
+    }
+
+    for model, data in GENERATION_RESULTS.items():
+        category = data.get("mult")
+        scores = data.get("w_ceb")
+
+        kshot = list(scores.keys())
+        genscore = list(scores.values())
+        ax.plot(
+            kshot,
+            genscore,
+            color=colors.get(category),
+            label=category,
+            marker="o",
+            linestyle="--",
+        )
+
+    ax.set_xticks([0, 1, 3, 5])
+    ax.set_xlabel("Number of demonstrations ($k$-shot)")
+    ax.set_ylabel("Generation Score")
+    ax.set_ylim(top=35)
+
+    output_path = Path(args.output_path)
+
+    plt.tight_layout()
+
+    fig.savefig(output_path, bbox_inches="tight")
+    if args.svg:
+        print("Also saving an SVG version")
+        fig.savefig(output_path.with_suffix(".svg"), bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    main()
