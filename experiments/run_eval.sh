2b8648ee14f0df584525585eccba9b4e7ed4c8b9
Lester James Miranda <ljvmiranda@gmail.com>
2026-01-20 09:25:38 +0000

    Add ablations file and experiments

---
+++ experiments/run_eval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REVISION=$1
+HF_ORG="ljvmiranda921"
+
+cd lighteval
+# For models in HuggingFace and accessible via vLLM
+cat examples/tasks/all_filbench_tasks.txt | xargs -I {} \
+    python -m lighteval vllm "pretrained=ljvmiranda921/msde-sft-dev,revision=$REVISION" {} \
+    --push-to-hub \
+    --results-org $HF_ORG \
+    --custom-tasks community_tasks/filbench_evals.py \
+    --max-samples 1000
